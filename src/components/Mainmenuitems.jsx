3c311499b54cf730749a0576911b55ece213885f
Shaid Islam <shaid85@gmail.com>
2024-02-10 01:49:36 +0600

    mobile menu

--- src/components/Mainmenuitems.jsx
+++ src/components/Mainmenuitems.jsx
@@ -71,7 +71,7 @@ const Mainmenuitems = () => {
                             <Link href="/login" className="text-gray-800 dark:text-white font-medium text-sm px-2 lg:px-3 py-2 lg:py-2.5 mr-2 lg:mr-0 focus:outline-none">
                                 Login
                             </Link>                    
-                            <Link href="/signup" className="text-gray-800 dark:text-white font-medium text-sm px-2 lg:px-3 py-2 lg:py-2.5 mr-2 lg:mr-0 focus:outline-none">
+                            <Link href="/signup" className="hidden sm:inline-block text-gray-800 dark:text-white font-medium text-sm px-2 lg:px-3 py-2 lg:py-2.5 mr-2 lg:mr-0 focus:outline-none">
                                 Signup
                             </Link> 
                                 </>
